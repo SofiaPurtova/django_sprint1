b52bd3b0d27b067aaaa87dcd210f75b67dbd13f7
SofiaPurtova <sofiapurtova@yandex.ru>
2025-04-30 23:00:59 +0300

    the code is aligned with pep8

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -4,7 +4,8 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Главная страница
-    path('posts/<int:id>/', views.post_detail, name='post_detail'),  # Страница поста
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),  # Категории
+    path('', views.index, name='index'),
+    path('posts/<int:id>/', views.post_detail, name='post_detail'),
+    path('category/<slug:category_slug>/',
+         views.category_posts, name='category_posts'),
 ]
